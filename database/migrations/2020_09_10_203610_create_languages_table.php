f749a1c4737cd00964cfc5b0a6019b19776af15c
Nizar El Berjawi <nizarberjawi12@gmail.com>
2020-09-11 07:36:08 +1000

    Create Language model and migration

---
+++ database/migrations/2020_09_10_203610_create_languages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+            $table->string('iso639_1')->unique();
+            $table->string('iso639_2')->unique();
+            $table->string('name');
+            $table->string('native_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('languages');
+    }
+}
