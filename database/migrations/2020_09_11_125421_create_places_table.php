1257adc270a4fc3355eac1c18b6f7704a31c1101
Nizar El Berjawi <nizarberjawi12@gmail.com>
2020-09-13 07:40:45 +1000

    Create Place  model and migration

---
+++ database/migrations/2020_09_11_125421_create_places_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('population');
+            $table->integer('area');
+            $table->string('elevation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
