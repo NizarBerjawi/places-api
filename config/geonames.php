1e57775da238aac0f0a149b09e637197553be274
Nizar El Berjawi <nizarberjawi12@gmail.com>
2020-09-17 11:09:16 +0000

    Minor refactor and install DOMCrawler

--- config/geonames.php
+++ config/geonames.php
@@ -11,4 +11,25 @@ return [
     |
     */
     'url' => env('GEONAMES_URL', 'https://download.geonames.org/export/dump'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Country flags base url
+    |--------------------------------------------------------------------------
+    |
+    | The URL of country flags
+    |
+    */
+    'flags' => env('FLAGS_API_URL', 'https://img.geonames.org/flags/x'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Feature codes base url
+    |--------------------------------------------------------------------------
+    |
+    | The URL of feature codes
+    |
+    */
+    'feature_codes' => env('FEATURE_CODE_URL', 'http://www.geonames.org/export/codes.html'),
+
 ];
