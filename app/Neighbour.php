47a1c6268a71c46533764514fd7458fc0c0ad899
Nizar El Berjawi <nizarberjawi12@gmail.com>
2020-10-06 10:48:33 +1000

    Create Neighbour model and relation

---
+++ app/Neighbour.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Neighbour extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'country_neighbour';
+}
