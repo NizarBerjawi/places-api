723ea64db37e085826a0be752378d698159f84f5
Nizar El Berjawi <nizarberjawi12@gmail.com>
2020-10-13 21:16:42 +0000

    Update Grumphp configuration

--- app/Currency.php
+++ app/Currency.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Currency extends Model
 {
     /**
-    * The attributes that are mass assignable.
-    *
-    * @var array
-    */
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name', 'code', 'symbol'
     ];
