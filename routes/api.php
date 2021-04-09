9041eb76112f904d07a509fc1c159b070d1c0d33
Nizar El Berjawi <nizarberjawi12@gmail.com>
2021-04-09 06:39:26 +0000

    Finalize CountryTimeZone controller

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::group(['prefix' => 'countries'], function () {
     Route::get('/{country}/currency', 'CountryCurrencyController@index');
     Route::get('/{country}/languages', 'CountryLanguageController@index');
     Route::get('/{country}/places', 'CountryPlacesController@index');
+    Route::get('/{country}/timeZones', 'CountryTimeZoneController@index');
 });
 
 Route::group(['prefix' => 'continents'], function () {
