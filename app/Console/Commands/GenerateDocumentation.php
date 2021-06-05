1c060e4bf1b8f7a7faa92ff4309121bdcaf83e4e
Nizar El Berjawi <nizarberjawi12@gmail.com>
2021-06-05 12:10:56 +0000

    Create Job to generate OpenApi documentation

---
+++ app/Console/Commands/GenerateDocumentation.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use OpenApi\Generator;
+
+class GenerateDocumentation extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'docs:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate up-to-date API documentation';
+
+    /**
+     * Execute the console command.
+     *
+     * @param  \App\Support\DripEmailer  $drip
+     * @return mixed
+     */
+    public function handle()
+    {
+        $openApi = Generator::scan(['app/Http/Controllers/Api']);
+        $content = $openApi->toJson();
+
+        (new Filesystem)->put(base_path('openApi.json'), $content);
+    }
+}
