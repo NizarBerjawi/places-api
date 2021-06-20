23944c5dafbd701b362eaad20ace4a683a352f75
Nizar El Berjawi <nizarberjawi12@gmail.com>
2021-06-20 08:06:30 +0000

    Remove api-spec

--- app/Http/Controllers/Api/V1/api-spec.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-/*
- * @OA\Info(title="Places API", version="1.0.0")
- */
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 
+/**
+ * Places API v1.
+ *
+ * @OA\Info(title="Places API", version="1.0.0")
+ */
 class Controller extends BaseController
 {
     //
