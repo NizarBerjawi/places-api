4e6ed7cbd7bfa7a6c15fa838a76edb0797b44133
Nizar El Berjawi <nizarberjawi12@gmail.com>
2021-06-26 00:14:49 +0000

    Update the introduction page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="content has-text-centered">
+    <p>
+        <strong>Places API</strong> by
+        <a href="https://github.com/NizarBerjawi/countries-api">
+            Nizar El Berjawi
+        </a>.
+    </p>
+    <p>Copyright &#169; {{ \Carbon\Carbon::now()->format('Y') }}</p>
+</div>
