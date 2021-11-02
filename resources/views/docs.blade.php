de0b98e19389d50962b6253ef6b5028d3dd0fe5c
Nizar El Berjawi <nizarberjawi12@gmail.com>
2021-11-02 02:56:18 +0000

    Create Volumes for ssl certificates

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -7,9 +7,9 @@
 @section('content')
     <div id='swagger'></div>
 
-    @if (config('app.env') === 'production')
+    <!-- @if (config('app.env') === 'production')
         <img src="https://validator.swagger.io/validator?url={{ url('openApi.json') }}">
-    @endif
+    @endif -->
 @endsection
 
 @section('scripts')
