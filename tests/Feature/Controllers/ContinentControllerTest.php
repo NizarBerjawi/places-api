726825006e6d470bf4dbd3c3decae378d643f037
Nizar El Berjawi <nizarberjawi12@gmail.com>
2022-11-29 21:52:45 +0000

    Install Pest

---
+++ tests/Feature/Controllers/ContinentControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\Continent;
+
+use function Pest\Laravel\get;
+use function Pest\Laravel\getJson;
+
+test('returns correct structure on GET continents', function () {
+    getJson('/api/v1/continents')->assertJsonStructure([
+        'data' => [ [ 'code', 'name' ] ],
+        'links' => [ 'prev', 'next' ],
+        'meta' => [ 'path', 'perPage', 'nextCursor', 'prevCursor' ],
+    ]);
+});
+
+test('returns 200 response on GET continents', function () {
+    getJson('/api/v1/continents')->assertOk();
+});
+
+test('returns correct pagination limit on GET continents', function() {
+    $response = getJson('/api/v1/continents');
+
+    $continentsCount = Continent::count();
+    // $continentsData = json_decode($>response->getContent(), true);
+
+    if ($continentsCount < config('geonames.pagination_limit')) {
+        $this->assertEquals(
+            count(Arr::get($response, 'data')),
+            $continentsCount
+        );
+    } else {
+        $this->assertEquals(
+            count(Arr::get($response, 'data')),
+            config('geonames.pagination_limit')
+        );
+    }
+
+    $this->assertEquals(
+        Arr::get($response, 'meta.per_page'),
+        config('geonames.pagination_limit')
+    );
+});
