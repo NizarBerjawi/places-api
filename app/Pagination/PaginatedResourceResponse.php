0841b1e406c3864dc9e4942b4e1990d54cfd2311
Nizar El Berjawi <nizarberjawi12@gmail.com>
2023-09-10 11:55:53 +0000

    Update pagination links for Rapid API

--- app/Pagination/PaginatedResourceResponse.php
+++ app/Pagination/PaginatedResourceResponse.php
@@ -3,6 +3,7 @@
 namespace App\Pagination;
 
 use Illuminate\Http\Resources\Json\PaginatedResourceResponse as IlluminateJsonPaginatedResourceResponse;
+use Illuminate\Support\Arr;
 
 class PaginatedResourceResponse extends IlluminateJsonPaginatedResourceResponse
 {
@@ -14,16 +15,40 @@ class PaginatedResourceResponse extends IlluminateJsonPaginatedResourceResponse
      */
     protected function paginationInformation($request)
     {
+        $path = $request->url();
+        $query = $request->query();
         $paginated = $this->resource->resource->toArray();
+        $apiVersion = config('api.version');
+
+        $next = $paginated['next_page_url'] ?? null;
+        $prev = $paginated['prev_page_url'] ?? null;
+
+        // We customize pagination links for people accessing Places API
+        // through the RapidAPI proxy
+        if ($request->hasHeader('X-RapidAPI-Host')) {
+            $path = $request->getScheme().'://'.$request->header('X-RapidAPI-Host').'/'.str_replace("api/$apiVersion/", '', $request->path());
+
+            if (isset($paginated['prev_cursor'])) {
+                $query['page']['cursor'] = $paginated['prev_cursor'];
+
+                $prev = $path.'?'.Arr::query($query);
+            }
+
+            if (isset($paginated['next_cursor'])) {
+                $query['page']['cursor'] = $paginated['next_cursor'];
+
+                $next = $path.'?'.Arr::query($query);
+            }
+        }
 
         return [
             'links' => [
-                'prev' => $paginated['prev_page_url'] ?? null,
-                'next' => $paginated['next_page_url'] ?? null,
+                'prev' => $prev,
+                'next' => $next,
             ],
             'meta' => [
                 'nextCursor' => $paginated['next_cursor'] ?? null,
-                'path' => $request->url(),
+                'path' => $path,
                 'perPage' => $paginated['per_page'] ?? null,
                 'prevCursor' => $paginated['prev_cursor'] ?? null,
             ],
